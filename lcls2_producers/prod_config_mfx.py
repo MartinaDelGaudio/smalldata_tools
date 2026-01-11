05b6aef9493fcd455330f0562ff1e6cc4e154ad6
Martina Del Gaudio <martina.delgaudio@studbocconi.it>
2026-01-11 11:17:06 -0800

    prod config mfx removing get_azav

--- lcls2_producers/prod_config_mfx.py
+++ lcls2_producers/prod_config_mfx.py
@@ -91,7 +91,7 @@ def get_azav(run):
         az_dict["phiBins"] = 11  # Number of phi bins for azint
         az_dict["qbin"] = 0.025  # Bin width in q for az int
         az_dict["userMask"] = None
-        ret_dict["jungfrau"] = az_dict
+        # ret_dict["jungfrau"] = az_dict
 
     return ret_dict
 
